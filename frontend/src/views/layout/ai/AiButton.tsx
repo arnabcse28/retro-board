1d2ee9fa3d9166eac4b09d6b8cc594f11da5c2c5
Antoine Jaussoin <antoine@jaussoin.com>
2023-03-26 20:44:25 +0100

    trans

--- frontend/src/views/layout/ai/AiButton.tsx
+++ frontend/src/views/layout/ai/AiButton.tsx
@@ -1,5 +1,5 @@
 import { Psychology } from '@mui/icons-material';
-import { Button } from '@mui/material';
+import { Button, IconButton, useMediaQuery } from '@mui/material';
 import useModal from 'hooks/useModal';
 import { useTranslation } from 'react-i18next';
 import { AiCoach } from './AiCoach';
@@ -7,17 +7,24 @@ import { AiCoach } from './AiCoach';
 export function AiButton() {
   const [opened, open, close] = useModal();
   const { t } = useTranslation();
+  const small = useMediaQuery('(max-width:600px)');
   return (
     <>
-      <Button
-        onClick={open}
-        variant="contained"
-        color="secondary"
-        startIcon={<Psychology />}
-        style={{ marginRight: 20 }}
-      >
-        {t('Ai.title')}
-      </Button>
+      {small ? (
+        <IconButton onClick={open} color="secondary">
+          <Psychology />
+        </IconButton>
+      ) : (
+        <Button
+          onClick={open}
+          variant="contained"
+          color="secondary"
+          startIcon={<Psychology />}
+          style={{ marginRight: 20 }}
+        >
+          {t('Ai.title')}
+        </Button>
+      )}
       <AiCoach onClose={close} open={opened} />
     </>
   );
