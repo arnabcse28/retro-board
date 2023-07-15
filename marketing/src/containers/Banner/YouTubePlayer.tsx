e2e62eea00e707dcef54d83b205105d4c861dc77
Antoine Jaussoin <antoine@jaussoin.com>
2023-07-15 21:23:50 +0100

    You tube player

---
+++ marketing/src/containers/Banner/YouTubePlayer.tsx
@@ -0,0 +1,80 @@
+import styled from 'styled-components';
+
+type YouTubePlayerProps = {
+  url: string;
+  onClose: () => void;
+};
+
+export function YouTubePlayer({ onClose, url }: YouTubePlayerProps) {
+  return (
+    <Player>
+      <HeaderContainer>
+        <PlayerHeader>
+          <svg
+            onClick={onClose}
+            width="70px"
+            height="70px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12 2C6.49 2 2 6.49 2 12C2 17.51 6.49 22 12 22C17.51 22 22 17.51 22 12C22 6.49 17.51 2 12 2ZM15.36 14.3C15.65 14.59 15.65 15.07 15.36 15.36C15.21 15.51 15.02 15.58 14.83 15.58C14.64 15.58 14.45 15.51 14.3 15.36L12 13.06L9.7 15.36C9.55 15.51 9.36 15.58 9.17 15.58C8.98 15.58 8.79 15.51 8.64 15.36C8.35 15.07 8.35 14.59 8.64 14.3L10.94 12L8.64 9.7C8.35 9.41 8.35 8.93 8.64 8.64C8.93 8.35 9.41 8.35 9.7 8.64L12 10.94L14.3 8.64C14.59 8.35 15.07 8.35 15.36 8.64C15.65 8.93 15.65 9.41 15.36 9.7L13.06 12L15.36 14.3Z"
+              fill="#292D32"
+            />
+          </svg>
+        </PlayerHeader>
+      </HeaderContainer>
+      <iframe
+        width="100%"
+        height="100%"
+        src={url}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </Player>
+  );
+}
+
+const Player = styled.div`
+  position: fixed;
+  top: 2%;
+  bottom: 2%;
+  right: 2%;
+  left: 2%;
+  width: 96%;
+  height: 96%;
+  z-index: 10000;
+  background-color: black;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+    rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+    rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+`;
+
+const HeaderContainer = styled.div`
+  height: 0;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const PlayerHeader = styled.div`
+  position: relative;
+  padding-top: 5px;
+  padding-right: 5px;
+  cursor: pointer;
+
+  svg {
+    path {
+      fill: hsl(0, 100%, 70%);
+    }
+  }
+
+  &:hover {
+    svg {
+      path {
+        fill: red;
+      }
+    }
+  }
+`;
