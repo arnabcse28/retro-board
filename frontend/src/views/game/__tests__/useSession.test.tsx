3a2cd503ab2ea228855ed4aeedd04dd0bf350fed
Antoine Jaussoin <antoine@jaussoin.com>
2023-09-24 08:32:08 +0100

    test

--- frontend/src/views/game/__tests__/useSession.test.tsx
+++ frontend/src/views/game/__tests__/useSession.test.tsx
@@ -36,16 +36,16 @@ describe('useSession', () => {
     expect(context.current.session).not.toBe(null);
   });
 
-  // describe('Renaming a session', () => {
-  //   it('Should rename the session correctly', () => {
-  //     const context = render();
-  //     expect(context.current.session?.name).toBe('My Retro');
-  //     act(() => {
-  //       context.current.renameSession('Something else');
-  //     });
-  //     expect(context.current.session?.name).toBe('Something else');
-  //   });
-  // });
+  describe('Renaming a session', () => {
+    it('Should rename the session correctly', () => {
+      const context = render();
+      expect(context.current.session?.name).toBe('My Retro');
+      act(() => {
+        context.current.editSessionSettings({ name: 'Something else' });
+      });
+      expect(context.current.session?.name).toBe('Something else');
+    });
+  });
 
   describe('Resetting a session', () => {
     it('Should set the session to null', () => {
